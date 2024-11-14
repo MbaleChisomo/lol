f29a7aafd1cf5a602f8cf9520dc4815916a9c11e
nachipo <nachiponicole24@gmail.com>
2024-11-14 16:34:22 +0200

    Update Question1.py

--- Question1.py
+++ Question1.py
@@ -5,5 +5,5 @@ temp = x
 x = y
 y = temp
 
-print(f"the value of x after swapping:",{x})
-print(f"the value of y after swapping:",{y})
+print(f"the value of x after swapping:",format.{x})
+print(f"the value of y after swapping:",format.{y})
